40af978dfadbd21215253488d3f2487cdb6bbded
Santiago Alexis Valencia Zapata <svalenciaz@unal.edu.co>
2021-11-04 17:26:33 -0500

    Project running

--- run-app/run-backend.sh
+++ run-app/run-backend.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
-echo "Hello from backend!"
+echo "Running backend!"
+
+# Calling enviromental variables defined in te Vagrantfile
+sudo echo 'export PORT=$PORT' >> /etc/profile
+sudo echo 'export IPSTACK_API_KEY=$IPSTACK_API_KEY' >> /etc/profile
+sudo echo 'export WEATHER_API_KEY=$WEATHER_API_KEY' >> /etc/profile
+
+# Create a directory for locate the server
+rm -rf /project
+mkdir -p /project/server
+
+# Copy the server directory into project
+cp -R /shared/server /project/server
+
+cd /project/server
+
+# Run app in background
+nohup ./server/vuego-demoapp &
--- run-app/run-frontend.sh
+++ run-app/run-frontend.sh
@@ -1,3 +1,53 @@
 #!/usr/bin/env bash
-echo "Hello from frontend!"
+echo "Running frontend!"
 
+# Install nginx
+yum install nginx -y
+
+# Enable nginx daemon
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+# Copy and decompress front.tar.gz from 'shared' directory
+rm -rf /frontend
+mkdir /frontend
+cp /shared/front.tar.gz /frontend/front.tar.gz
+cd /frontend
+tar -zxf front.tar.gz
+
+# Modify nginx file with Vue documentation example, replacing root dir
+cat <<-'default_config' > /etc/nginx/nginx.conf
+user  nginx;
+worker_processes  1;
+error_log  /var/log/nginx/error.log warn;
+pid        /var/run/nginx.pid;
+events {
+  worker_connections  1024;
+}
+http {
+  include       /etc/nginx/mime.types;
+  default_type  application/octet-stream;
+  log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                    '$status $body_bytes_sent "$http_referer" '
+                    '"$http_user_agent" "$http_x_forwarded_for"';
+  access_log  /var/log/nginx/access.log  main;
+  sendfile        on;
+  keepalive_timeout  65;
+  server {
+    listen       80;
+    server_name  localhost;
+    location / {
+      root   /frontend/dist;
+      index  index.html;
+      try_files $uri $uri/ /index.html;
+    }
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+      root   /usr/share/nginx/html;
+    }
+  }
+}
+default_config
+
+# Reload daemons after document modification
+sudo systemctl reload nginx
